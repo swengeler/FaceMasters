c0f0ad4809c117ccc1a59787267c5c89306a2bd7
Patrick Larsen-Ledet <lapatric@student.ethz.ch>
2019-05-22 13:15:47 +0200

    implemented basic gui

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -5,7 +5,7 @@
 #include <imgui/imgui.h>
 #include <iostream>
 #include <igl/unproject_onto_mesh.h>
-#include <string> 
+#include <string>
 #include <igl/is_file.h>
 
 using namespace Eigen;
@@ -13,7 +13,7 @@ using namespace std;
 
 
 typedef igl::opengl::glfw::Viewer Viewer;
-
+Viewer viewer;
 
 bool callback_pre_draw(Viewer& viewer);
 
@@ -21,28 +21,44 @@ bool callback_pre_draw(Viewer& viewer);
 MatrixXd V_Faces(0,0);
 MatrixXi F;
 
+// solver for PCA
+EigenSolver<MatrixXd> eig;
+
+// number of eigenfaces to use
+int noEigenfaces = 5;
+// average face (dim: #Vx3)
+MatrixXd averageFace;
+// eigenfaces stored column-wise
+MatrixXd V_Eigenfaces;
+// weights of eigenfaces controlled by sliders
+vector<float> eigenFaceWeights(noEigenfaces, 0.0f);
+
+
 void readFaceAndAddToFaces(string face) {
     cout << "loading face: " << face << endl;
     MatrixXd V_tmp;
     MatrixXi F_tmp;
     igl::readOBJ(face, V_tmp, F_tmp);
+    // all F_tmp should be the same
+    F = F_tmp;
     // eigen stores the values in column by column format
-    // so to get row by row we have to transpose first 
+    // so to get row by row we have to transpose first
     V_tmp.transposeInPlace();
-    // then flatten the matrix row by row 
+    // then flatten the matrix row by row
     VectorXd V_row_temp(Map<VectorXd>(V_tmp.data(), V_tmp.cols()*V_tmp.rows()));
-    // and add it as column to the V_faces
+    // and add it as column to the V_Faces
     V_Faces.conservativeResize(V_row_temp.rows(), V_Faces.cols() + 1);
     V_Faces.col(V_Faces.cols() -1) = V_row_temp;
 }
 
+
 void readFaces(string baseDir) {
     cout << "Loading faces from: " << baseDir << endl;
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 9; i++) {
         string normal = baseDir + "/person" + to_string(i+1) + "_normal.obj";
         string smile = baseDir + "/person" + to_string(i+1) + "_smile.obj";
-        
+
         if (!igl::is_file(normal.c_str())) {
             cout << "face: " << normal << " not found, quitting loading" << endl;
             break;
@@ -55,26 +71,42 @@ void readFaces(string baseDir) {
     cout << "loaded: " << V_Faces.cols() << " faces" << " with " << V_Faces.rows() << " points each" << endl;
 }
 
+
 void pca() {
     // calculate average face
     VectorXd avg_face = V_Faces.rowwise().mean();
     // subtract average face
     V_Faces.colwise() -= avg_face;
 
-    // calculate eigenvectos and values on covariance matrix of faces
-    EigenSolver<MatrixXd> eig(V_Faces.transpose()*V_Faces); 
+    // calculate eigenvectors and values on covariance matrix of faces
+    eig = EigenSolver<MatrixXd>(V_Faces.transpose()*V_Faces);
     eig.eigenvalues();
     eig.eigenvectors();
 
     // this is the fast way normally XX' would be the cov. matrix so to get to the eigv of the cov matrix
-    // we have to multiply it by X again , maybe normalize eigv. if we need them normalized ? 
+    // we have to multiply it by X again , maybe normalize eigv. if we need them normalized ?
     for (int i = 0; i < eig.eigenvectors().cols(); i++) {
-        //VectorXd i_eigenvector = V_Faces*eig.eigenvectors().col(0);
+        //VectorXd i_eigenvector = V_Faces*eig.eigenvectors().col(i);
     }
-    
 
-
+    // compute eigenfaces and average face
+    V_Eigenfaces = V_Faces * eig.eigenvectors().real();
+    averageFace = Map<MatrixXd>(avg_face.data(), 3, V_Eigenfaces.rows()/3).transpose();
 }
+
+
+void drawComposedFace(){
+  MatrixXd composedFace = averageFace;
+
+  for (int i = 0; i < noEigenfaces; i++) {
+    MatrixXd eigenface = Map<MatrixXd>(V_Eigenfaces.col(i).data(), 3, V_Eigenfaces.rows()/3).transpose();
+    composedFace.noalias() += eigenFaceWeights[i] * eigenface;
+  }
+
+  // update visualised data
+  viewer.data().set_mesh(composedFace, F);
+}
+
 
 int main(int argc, char *argv[]) {
     string baseDir;
@@ -87,37 +119,38 @@ int main(int argc, char *argv[]) {
         baseDir = string(argv[1]);
         readFaces(baseDir);
         pca();
+        eigenFaceWeights[0] = 1.0f;
+        drawComposedFace();
     }
 
-    Viewer viewer;
     igl::opengl::glfw::imgui::ImGuiMenu menu;
     viewer.plugins.push_back(&menu);
-
+    viewer.core.align_camera_center(viewer.data().V);
+    viewer.data().show_lines = false;
 
     // Draw additional windows
-    menu.callback_draw_custom_window = [&]() {
-        ImGui::SetNextWindowPos(ImVec2(180.f * menu.menu_scaling(), 10), ImGuiSetCond_FirstUseEver);
-        ImGui::SetNextWindowSize(ImVec2(200, 160), ImGuiSetCond_FirstUseEver);
-        ImGui::Begin( "Selection Window", nullptr, ImGuiWindowFlags_NoSavedSettings );
+    menu.callback_draw_viewer_menu = [&]() {
 
+        // menu.draw_viewer_menu(); // draw default menu above
 
-        if (ImGui::Button("Save"))  {
+        // Add new group: Sliders for eigenfaces
+        if (ImGui::CollapsingHeader("Blending Eigenfaces", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+          for(int i=0; i<noEigenfaces; i++){
+            const string label = "eigenface " + to_string(i);
+            if (ImGui::SliderFloat(label.c_str(), &eigenFaceWeights[i], 0.0f, 1.0f)){
+              drawComposedFace();
+            }
+          }
         }
 
-        ImGui::End();
     };
 
     viewer.callback_pre_draw = callback_pre_draw;
-
     viewer.launch();
 }
 
 
-
 bool callback_pre_draw(Viewer& viewer) {
-
+  return false;
 }
-
-
-
-
